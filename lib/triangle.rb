cccf0aab0e0d29b20d7d2d12416bc68c48f04ea9
Pharia Le <pharia.le@gmail.com>
2020-03-19 04:59:03 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :length, :height, :width
+  
+  @@all = []
+  
+  def initialize(length, height, width)
+    @length = length
+    @height = height
+    @width = width
+    self.save
+  end
+  
+  def kind
+    if (@length+@height <= @width) || (@height+@width <= @length) || (@width+@length <= @height) || @height == 0 || @width == 0 || @length == 0
+        raise TriangleError
+    elsif @length == @height && @height == @width
+      :equilateral
+    elsif @length != @height && @height != @width && @width != @length
+      :scalene
+    else
+      :isosceles
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
